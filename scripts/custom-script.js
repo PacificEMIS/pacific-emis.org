0d27e1e4d6a7c7cade78cd756a3f5a8f1666f11e
Ghislain Hachey <ghachey@nuzusys.com>
2022-04-07 14:07:46 -0300

    Updates to websites, add contact form

--- scripts/custom-script.js
+++ scripts/custom-script.js
@@ -65,49 +65,72 @@ jQuery(function ($) {
     *=============================================================================================================*/
     /*1st Method
     * --------*/
-    $('a[href*=#]:not([href=#])').on('click', function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-        || location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-              scrollTop: target.offset().top - 0
-          }, 1000);
-          return false;
-        }
-      }
-    });
+    // $('a[href*=#]:not([href=#])').on('click', function() {
+    //   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    //     || location.hostname == this.hostname) {
+    //     var target = $(this.hash);
+    //     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    //     if (target.length) {
+    //       $('html,body').animate({
+    //           scrollTop: target.offset().top - 0
+    //       }, 1000);
+    //       return false;
+    //     }
+    //   }
+    // });
 
     /*2nd Method
     * --------*/
-    /*$("a").on('click', function(event) {
+    // $("a").on('click', function(event) {
 
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
+    //   // Make sure this.hash has a value before overriding default behavior
+    //   if (this.hash !== "") {
+    //     // Prevent default anchor click behavior
+    //     event.preventDefault();
 
-        // Store hash
-        var hash = this.hash;
+    //     // Store hash
+    //     var hash = this.hash;
 
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
+    //     // Using jQuery's animate() method to add smooth page scroll
+    //     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    //     $('html, body').animate({
+    //       scrollTop: $(hash).offset().top
+    //     }, 800, function(){
 
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });*/
+    //       // Add hash (#) to URL when done scrolling (default click behavior)
+    //       window.location.hash = hash;
+    //     });
+    //   } // End if
+    // });
 
     /*$('ul.tabs').tabs({
       swipeable : true,
       responsiveThreshold : 1920
-    });*/
+      });*/
 
+    /*Materialize Slider initialization
+    * ============================================================================================================*/
+      
+      // $('.slider').slider();
+
+    /*Materialize Carousel initialization
+    * ============================================================================================================*/
+      
+      // $('.carousel').carousel({
+      // 	  indicators: true
+      // });
+      $('.carousel.carousel-slider').carousel({
+	  // duration: 200,
+	  // dist: -100,
+	  // shift: 0,
+	  // padding: 0,
+	  // numVisible: 3,	  
+	  fullWidth: true,	  
+	  indicators: true,
+	  // noWrap: false,
+	  // onCycleTo: null
+      });
+      
   });
 });
 
